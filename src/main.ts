6dadb66fd7c2986cfa96824a70b894748d0329dd
Rafael Viktor <rafael@izzyway.com.br>
2025-07-19 18:02:34 -0300

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ZodValidationPipe } from 'nestjs-zod';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ZodValidationPipe());
+  app.enableCors(); 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
